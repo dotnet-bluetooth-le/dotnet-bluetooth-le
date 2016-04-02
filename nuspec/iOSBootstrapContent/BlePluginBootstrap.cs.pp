f7fa2536b67cb964b43b034317f82510806f04c0
sead <sead@zuehlke.com>
2016-04-02 10:39:43 +0200

    Updated to MvvmCross 4.1.0. Fixed namespace for ios bootstrap file.

--- nuspec/iOSBootstrapContent/BlePluginBootstrap.cs.pp
+++ nuspec/iOSBootstrapContent/BlePluginBootstrap.cs.pp
@@ -3,7 +3,7 @@ using MvvmCross.Platform.Plugins;
 namespace $rootnamespace$.Bootstrap
 {
     public class BlePluginBootstrap
-        : MvxLoaderPluginBootstrapAction<MvvmCross.Plugins.BLE.PluginLoader, MvvmCross.Plugins.BLE.Touch.Plugin>
+        : MvxLoaderPluginBootstrapAction<MvvmCross.Plugins.BLE.PluginLoader, MvvmCross.Plugins.BLE.iOS.Plugin>
     {
     }
 }
