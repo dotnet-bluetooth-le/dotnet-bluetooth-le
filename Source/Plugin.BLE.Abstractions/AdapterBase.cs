a6f669affc0c482d5558d585a5094969e5ce863c
Sven-Michael Stübe <stuebe2k7@gmail.com>
2016-04-14 22:52:43 +0200

    need this in the :office:

---
+++ Source/Plugin.BLE.Abstractions/AdapterBase.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Plugin.BLE.Abstractions.Contracts;
+
+namespace Plugin.BLE.Abstractions
+{
+    public abstract class AdapterBase : IAdapter
+    {
+        private CancellationTokenSource _scanCancellationTokenSource;
+        private readonly IList<IDevice> _discoveredDevices;
+        private readonly IList<IDevice> _connectedDevices;
+        private volatile bool _isScanning;
+
+        public event EventHandler<DeviceDiscoveredEventArgs> DeviceAdvertised = delegate { };
+        public event EventHandler<DeviceDiscoveredEventArgs> DeviceDiscovered = delegate { };
+        public event EventHandler<DeviceConnectionEventArgs> DeviceConnected = delegate { };
+        public event EventHandler<DeviceBondStateChangedEventArgs> DeviceBondStateChanged = delegate { };
+        public event EventHandler<DeviceConnectionEventArgs> DeviceDisconnected = delegate { };
+        public event EventHandler<DeviceConnectionEventArgs> DeviceConnectionLost = delegate { };
+        public event EventHandler<DeviceConnectionEventArgs> DeviceConnectionError = delegate { };
+        public event EventHandler ScanTimeoutElapsed = delegate { };
+
+        public bool IsScanning
+        {
+            get { return _isScanning; }
+            private set { _isScanning = value; }
+        }
+
+        public int ScanTimeout { get; set; } = 10000;
+
+        public virtual IList<IDevice> DiscoveredDevices
+        {
+            get { return _discoveredDevices; }
+        }
+
+        public virtual IList<IDevice> ConnectedDevices
+        {
+            get { return _connectedDevices; }
+        }
+
+        protected AdapterBase()
+        {
+            _discoveredDevices = new List<IDevice>();
+            _connectedDevices = new List<IDevice>();
+        }
+
+        public void StartScanningForDevices()
+        {
+            StartScanningForDevices(new Guid[0]);
+        }
+
+        public async void StartScanningForDevices(Guid[] serviceUuids)
+        {
+            if (IsScanning)
+            {
+                Trace.Message("Adapter: Already scanning!");
+                return;
+            }
+
+            IsScanning = true;
+            _scanCancellationTokenSource = new CancellationTokenSource();
+
+            try
+            {
+                await StartScanningForDevicesNativeAsync(serviceUuids, _scanCancellationTokenSource.Token);
+                await Task.Delay(ScanTimeout, _scanCancellationTokenSource.Token);
+                Trace.Message("Adapter: Scan timeout has elapsed.");
+                StopScan();
+                ScanTimeoutElapsed(this, new EventArgs());
+            }
+            catch (TaskCanceledException)
+            {
+                Trace.Message("Adapter: Scan was cancelled.");
+                StopScan();
+            }
+        }
+
+        private void StopScan()
+        {
+            Trace.Message("Adapter: Stopping the scan for devices.");
+            StopScanNative();
+
+            if (_scanCancellationTokenSource != null)
+            {
+                _scanCancellationTokenSource.Dispose();
+                _scanCancellationTokenSource = null;
+            }
+
+            IsScanning = false;
+        }
+
+        public void StopScanningForDevices()
+        {
+            if (_scanCancellationTokenSource != null && !_scanCancellationTokenSource.IsCancellationRequested)
+            {
+                _scanCancellationTokenSource.Cancel();
+            }
+            else
+            {
+                Trace.Message("Adapter: Already cancelled scan.");
+            }
+        }
+
+        protected void HandleDiscoveredDevice(IDevice device)
+        {
+            DeviceAdvertised(this, new DeviceDiscoveredEventArgs { Device = device });
+
+            // TODO (sms): check equality implementation of device
+            if (_discoveredDevices.Contains(device))
+                return;
+
+            _discoveredDevices.Add(device);
+            DeviceDiscovered(this, new DeviceDiscoveredEventArgs { Device = device });
+        }
+
+        protected void HandleConnectedDevice(IDevice device)
+        {
+            DeviceConnected(this, new DeviceConnectionEventArgs { Device = device });
+        }
+
+        protected abstract Task StartScanningForDevicesNativeAsync(Guid[] serviceUuids, CancellationToken scanCancellationToken);
+        protected abstract void StopScanNative();
+        public abstract void ConnectToDevice(IDevice device, bool autoconnect = false);
+        public abstract void CreateBondToDevice(IDevice device);
+        public abstract void DisconnectDevice(IDevice device);
+    }
+}
--- Source/Plugin.BLE.iOS/Adapter.cs
+++ Source/Plugin.BLE.iOS/Adapter.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
@@ -11,21 +12,22 @@ using Plugin.BLE.Abstractions.Contracts;
 
 namespace Plugin.BLE.iOS
 {
-    public class Adapter : IAdapter
+    public class Adapter : AdapterBase
     {
-        //public event EventHandler<DeviceConnectionEventArgs> DeviceFailedToConnect = delegate { };
-        //public event EventHandler ConnectTimeoutElapsed = delegate { };
-
         private readonly AutoResetEvent _stateChanged = new AutoResetEvent(false);
-        private CancellationTokenSource _cancellationTokenSource;
-        private volatile bool _isScanning; //ToDo maybe lock
+        public CBCentralManager Central { get; }
+
+        /// <summary>
+        /// Registry used to store device instances for pending operations : disconnect
+        /// Helps to detect connection lost events.
+        /// </summary>
+        private readonly IDictionary<string, IDevice> _deviceOperationRegistry = new ConcurrentDictionary<string, IDevice>();
+        private readonly IDictionary<string, IDevice> _deviceConnectionRegistry = new ConcurrentDictionary<string, IDevice>();
+
+        public IList<IDevice> ConnectedDevices => _deviceConnectionRegistry.Values.ToList();
 
         public Adapter()
         {
-            ScanTimeout = 10000;
-            DeviceOperationRegistry = new Dictionary<string, IDevice>();
-            DeviceConnectionRegistry = new Dictionary<string, IDevice>();
-
             Central = new CBCentralManager(DispatchQueue.CurrentQueue);
 
             Central.DiscoveredPeripheral += (sender, e) =>
@@ -39,15 +41,8 @@ namespace Plugin.BLE.iOS
                     name = ((NSString)e.AdvertisementData.ValueForKey(CBAdvertisement.DataLocalNameKey)).ToString();
                 }
 
-                var d = new Device(e.Peripheral, name, e.RSSI.Int32Value, ParseAdvertismentData(e.AdvertisementData));
-
-                DeviceAdvertised(this, new DeviceDiscoveredEventArgs { Device = d });
-                if (ContainsDevice(DiscoveredDevices, e.Peripheral))
-                {
-                    return;
-                }
-                DiscoveredDevices.Add(d);
-                DeviceDiscovered(this, new DeviceDiscoveredEventArgs { Device = d });
+                var device = new Device(e.Peripheral, name, e.RSSI.Int32Value, ParseAdvertismentData(e.AdvertisementData));
+                HandleDiscoveredDevice(device);
             };
 
             Central.UpdatedState += (sender, e) =>
@@ -64,18 +59,17 @@ namespace Plugin.BLE.iOS
                 var guid = ParseDeviceGuid(e.Peripheral).ToString();
 
                 IDevice device;
-                if (DeviceOperationRegistry.TryGetValue(guid, out device))
+                if (_deviceOperationRegistry.TryGetValue(guid, out device))
                 {
-                    DeviceOperationRegistry.Remove(guid);
+                    _deviceOperationRegistry.Remove(guid);
                 }
 
                 //ToDo use the same instance of the device just update 
                 var d = new Device(e.Peripheral, e.Peripheral.Name, e.Peripheral.RSSI?.Int32Value ?? 0, device?.AdvertisementRecords.ToList() ?? new List<AdvertisementRecord>());
 
-                DeviceConnectionRegistry[guid] = d;
+                _deviceConnectionRegistry[guid] = d;
 
-                // raise our connected event
-                DeviceConnected(sender, new DeviceConnectionEventArgs { Device = d });
+                HandleConnectedDevice(d);
             };
 
             Central.DisconnectedPeripheral += (sender, e) =>
@@ -91,16 +85,16 @@ namespace Plugin.BLE.iOS
                 IDevice foundDevice;
 
                 // normal disconnect (requested by user)
-                var isNormalDisconnect = DeviceOperationRegistry.TryGetValue(stringId, out foundDevice);
+                var isNormalDisconnect = _deviceOperationRegistry.TryGetValue(stringId, out foundDevice);
                 if (isNormalDisconnect)
                 {
-                    DeviceOperationRegistry.Remove(stringId);
+                    _deviceOperationRegistry.Remove(stringId);
                 }
 
                 // remove from connected devices
-                if (DeviceConnectionRegistry.TryGetValue(stringId, out foundDevice))
+                if (_deviceConnectionRegistry.TryGetValue(stringId, out foundDevice))
                 {
-                    DeviceConnectionRegistry.Remove(stringId);
+                    _deviceConnectionRegistry.Remove(stringId);
                 }
 
                 if (isNormalDisconnect)
@@ -129,135 +123,50 @@ namespace Plugin.BLE.iOS
             };
         }
 
-        public CBCentralManager Central { get; }
-        
-        /// <summary>
-        ///     Registry used to store device instances for pending operations : disconnect
-        ///     Helps to detect connection lost events
-        /// </summary>
-        public Dictionary<string, IDevice> DeviceOperationRegistry { get; set; }
-
-        public Dictionary<string, IDevice> DeviceConnectionRegistry { get; set; }
-        // events
-        public event EventHandler<DeviceDiscoveredEventArgs> DeviceAdvertised = delegate { };
-        public event EventHandler<DeviceDiscoveredEventArgs> DeviceDiscovered = delegate { };
-        public event EventHandler<DeviceConnectionEventArgs> DeviceConnected = delegate { };
-        public event EventHandler<DeviceBondStateChangedEventArgs> DeviceBondStateChanged = delegate { };
-        public event EventHandler<DeviceConnectionEventArgs> DeviceDisconnected = delegate { };
-        public event EventHandler<DeviceConnectionEventArgs> DeviceConnectionLost = delegate { };
-        public event EventHandler<DeviceConnectionEventArgs> DeviceConnectionError = delegate { };
-        public event EventHandler ScanTimeoutElapsed = delegate { };
-
-        public bool IsScanning => _isScanning;
-
-        public int ScanTimeout { get; set; }
-
-        public IList<IDevice> DiscoveredDevices { get; private set; } = new List<IDevice>();
-
-        public IList<IDevice> ConnectedDevices => DeviceConnectionRegistry.Values.ToList();
-
-        public void StartScanningForDevices()
+        protected override async Task StartScanningForDevicesNativeAsync(Guid[] serviceUuids, CancellationToken scanCancellationToken)
         {
-            StartScanningForDevices(new Guid[] { });
+            // Wait for the PoweredOn state
+            await WaitForState(CBCentralManagerState.PoweredOn, scanCancellationToken).ConfigureAwait(false);
+
+            Trace.Message("Adapter: Starting a scan for devices.");
+
+            CBUUID[] serviceCbuuids = null;
+            if (serviceUuids != null && serviceUuids.Any())
+            {
+                serviceCbuuids = serviceUuids.Select(u => CBUUID.FromString(u.ToString())).ToArray();
+                Trace.Message("Adapter: Scanning for " + serviceCbuuids.First());
+            }
+
+            // TODO (sms): clear out the list ?? AdapterBase??
+            // DiscoveredDevices = new List<IDevice>();
+
+            Central.ScanForPeripherals(serviceCbuuids);
         }
 
-        public async void StartScanningForDevices(Guid[] serviceUuids)
+        protected override void StopScanNative()
         {
-            if (_isScanning)
-            {
-                Trace.Message("Adapter: Already scanning!");
-                return;
-            }
-
-            _isScanning = true;
-
-            // in ScanTimeout seconds, stop the scan
-            _cancellationTokenSource = new CancellationTokenSource();
-
-            try
-            {
-                // Wait for the PoweredOn state
-                await WaitForState(CBCentralManagerState.PoweredOn, _cancellationTokenSource.Token).ConfigureAwait(false);
-
-                Trace.Message("Adapter: Starting a scan for devices.");
-
-                CBUUID[] serviceCbuuids = null;
-                if (serviceUuids != null && serviceUuids.Any())
-                {
-                    serviceCbuuids = serviceUuids.Select(u => CBUUID.FromString(u.ToString())).ToArray();
-                    Trace.Message("Adapter: Scanning for " + serviceCbuuids.First());
-                }
-
-                // clear out the list
-                DiscoveredDevices = new List<IDevice>();
-
-                // start scanning
-                Central.ScanForPeripherals(serviceCbuuids);
-
-                await Task.Delay(ScanTimeout, _cancellationTokenSource.Token);
-
-                Trace.Message("Adapter: Scan timeout has elapsed.");
-
-                StopScan();
-
-                TryDisposeToken();
-                _isScanning = false;
-
-                //important for this to be caled after _isScanning = false so don't move to finally block
-                ScanTimeoutElapsed(this, new EventArgs());
-            }
-            catch (TaskCanceledException)
-            {
-                Trace.Message("Adapter: Scan was cancelled.");
-                StopScan();
-
-                TryDisposeToken();
-                _isScanning = false;
-            }
+            Central.StopScan();
         }
 
-        private void TryDisposeToken()
-        {
-            if (_cancellationTokenSource == null)
-                return;
-
-            _cancellationTokenSource.Dispose();
-            _cancellationTokenSource = null;
-        }
-
-        public void StopScanningForDevices()
-        {
-            if (_cancellationTokenSource != null && !_cancellationTokenSource.IsCancellationRequested)
-            {
-                _cancellationTokenSource.Cancel();
-            }
-            else
-            {
-                Trace.Message("Adapter: Already cancelled scan.");
-            }
-        }
-
-        public void ConnectToDevice(IDevice device, bool autoconnect)
+        public override void ConnectToDevice(IDevice device, bool autoconnect = false)
         {
             //ToDo autoconnect
-            DeviceOperationRegistry[device.ID.ToString()] = device;
+            _deviceOperationRegistry[device.ID.ToString()] = device;
             Central.ConnectPeripheral(device.NativeDevice as CBPeripheral, new PeripheralConnectionOptions());
         }
 
-        public void CreateBondToDevice(IDevice device)
+        public override void CreateBondToDevice(IDevice device)
         {
-            // ToDo
+            // TODO: not implemented
             DeviceBondStateChanged(this, new DeviceBondStateChangedEventArgs { Device = device, State = DeviceBondState.Bonded });
         }
 
-        public void DisconnectDevice(IDevice device)
+        public override void DisconnectDevice(IDevice device)
         {
             // update registry
-            DeviceOperationRegistry[device.ID.ToString()] = device;
+            _deviceOperationRegistry[device.ID.ToString()] = device;
             Central.CancelPeripheralConnection(device.NativeDevice as CBPeripheral);
         }
-
-
 
         private static Guid ParseDeviceGuid(CBPeripheral peripherial)
         {
@@ -272,12 +181,6 @@ namespace Plugin.BLE.iOS
             {
                 await Task.Run(() => _stateChanged.WaitOne(2000), cancellationToken).ConfigureAwait(false);
             }
-        }
-
-        private void StopScan()
-        {
-            Central.StopScan();
-            Trace.Message("Adapter: Stopping the scan for devices.");
         }
 
         private static bool ContainsDevice(IEnumerable<IDevice> list, CBPeripheral device)
