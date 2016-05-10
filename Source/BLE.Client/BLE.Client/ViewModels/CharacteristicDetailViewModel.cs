2835c0ca7f8b49d3ee08b1c8977b31132bc7784a
xabre <adri.sece@gmail.com>
2016-05-10 18:18:25 +0200

    Started charecteristic detail page for sample app.

--- Source/BLE.Client/BLE.Client/ViewModels/CharacteristicDetailViewModel.cs
+++ Source/BLE.Client/BLE.Client/ViewModels/CharacteristicDetailViewModel.cs
@@ -1,34 +1,116 @@
+using System;
+using System.Linq;
+using System.Text;
+using Acr.UserDialogs;
 using MvvmCross.Core.ViewModels;
+using Plugin.BLE.Abstractions;
 using Plugin.BLE.Abstractions.Contracts;
 
 namespace BLE.Client.ViewModels
 {
     public class CharacteristicDetailViewModel : BaseViewModel
     {
-        private ICharacteristic _characteristic;
+        private readonly IUserDialogs _userDialogs;
+        public ICharacteristic Characteristic { get; private set; }
 
-        public CharacteristicDetailViewModel(IAdapter adapter) : base(adapter)
+        public string CharacteristicValue => Characteristic?.Value.ToHexString().Replace("-", " ");
+
+        public string Permissions
         {
+            get
+            {
+                if (Characteristic == null)
+                    return string.Empty;
 
+                return (Characteristic.CanRead ? "Read " : "") +
+                       (Characteristic.CanWrite ? "Write " : "") +
+                       (Characteristic.CanUpdate ? "Update" : "");
+            }
+        }
+
+        public CharacteristicDetailViewModel(IAdapter adapter, IUserDialogs userDialogs) : base(adapter)
+        {
+            _userDialogs = userDialogs;
         }
 
         protected override async void InitFromBundle(IMvxBundle parameters)
         {
             base.InitFromBundle(parameters);
 
-            _characteristic = await GetCharacteristicFromBundleAsync(parameters);
+            Characteristic = await GetCharacteristicFromBundleAsync(parameters);
         }
 
         public override void Resume()
         {
             base.Resume();
 
-            if (_characteristic != null)
+            if (Characteristic != null)
             {
                 return;
             }
 
             Close(this);
+        }
+
+        public MvxCommand ReadCommand => new MvxCommand(ReadValueAsync);
+
+        private async void ReadValueAsync()
+        {
+            if (Characteristic == null)
+                return;
+
+            try
+            {
+                _userDialogs.ShowLoading("Reading characteristic value...");
+
+                await Characteristic.ReadAsync();
+
+                RaisePropertyChanged(() => CharacteristicValue);
+            }
+            catch (Exception ex)
+            {
+                _userDialogs.HideLoading();
+                _userDialogs.ShowError(ex.Message);
+
+            }
+            finally
+            {
+                _userDialogs.HideLoading();
+            }
+
+        }
+
+        public MvxCommand WriteCommand => new MvxCommand(WriteValueAsync);
+
+        private async void WriteValueAsync()
+        {
+            try
+            {
+                var result =
+                    await
+                        _userDialogs.PromptAsync("Input a value (as hex whitespace separated)", "Write value",
+                            placeholder: CharacteristicValue);
+
+                if (!result.Ok)
+                    return;
+
+                var data = GetBytes(result.Text);
+
+                _userDialogs.ShowLoading("Write characteristic value");
+                await Characteristic.WriteAsync(data);
+                _userDialogs.HideLoading();
+            }
+            catch (Exception ex)
+            {
+                _userDialogs.HideLoading();
+                _userDialogs.ShowError(ex.Message);
+            }
+            
+        }
+
+        private static byte[] GetBytes(string text)
+        {
+            return text.Split(' ').Select(token => Convert.ToByte(token)).ToArray();
         }
     }
 }
