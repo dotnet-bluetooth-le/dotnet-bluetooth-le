1f314efba7bb683d76a8628e1b6288d83cbd89b4
Sven-Michael Stübe <stuebe2k7@gmail.com>
2016-05-11 17:41:54 +0200

    added xml doc for IAdapter #21

--- Source/Plugin.BLE.Abstractions/Contracts/IAdapter.cs
+++ Source/Plugin.BLE.Abstractions/Contracts/IAdapter.cs
@@ -2,20 +2,40 @@ using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
-using Plugin.BLE.Abstractions.Exceptions;
 
 namespace Plugin.BLE.Abstractions.Contracts
 {
     public interface IAdapter
     {
-        // events
-        event EventHandler<DeviceDiscoveredEventArgs> DeviceAdvertised;
-        event EventHandler<DeviceDiscoveredEventArgs> DeviceDiscovered;
-        event EventHandler<DeviceConnectionEventArgs> DeviceConnected;
-        event EventHandler<DeviceConnectionEventArgs> DeviceDisconnected;
-        event EventHandler<DeviceConnectionEventArgs> DeviceConnectionLost;
+        /// <summary>
+        /// Occurs when the adapter receives an advertisement.
+        /// </summary>
+        event EventHandler<DeviceEventArgs> DeviceAdvertised;
+        /// <summary>
+        /// Occurs when the adapter recaives an advertisement for the first time of the current scan run.
+        /// This means once per every <see cref="StartScanningForDevicesAsync()"/> call. 
+        /// </summary>
+        event EventHandler<DeviceEventArgs> DeviceDiscovered;
+        /// <summary>
+        /// Occurs when a device has been connected.
+        /// </summary>
+        event EventHandler<DeviceEventArgs> DeviceConnected;
+        /// <summary>
+        /// Occurs when a device has been disconnected. This occurs on intendet disconnects after <see cref="DisconnectDeviceAsync"/>.
+        /// </summary>
+        event EventHandler<DeviceEventArgs> DeviceDisconnected;
+        /// <summary>
+        /// Occurs when a device has been disconnected. This occurs on unintendet disconnects (e.g. when the device exploded).
+        /// </summary>
+        event EventHandler<DeviceErrorEventArgs> DeviceConnectionLost;
+        /// <summary>
+        /// Occurs when the scan has been stopped due the timeout after <see cref="ScanTimeout"/> ms.
+        /// </summary>
         event EventHandler ScanTimeoutElapsed;
 
+        /// <summary>
+        /// Indicates, if the adapter is scanning for devices.
+        /// </summary>
         bool IsScanning { get; }
 
         /// <summary>
@@ -23,29 +43,70 @@ namespace Plugin.BLE.Abstractions.Contracts
         /// </summary>
         int ScanTimeout { get; set; }
         //IList<IDevice> DiscoveredDevices { get; }
+
+        /// <summary>
+        /// List of currently connected devices.
+        /// </summary>
         IList<IDevice> ConnectedDevices { get; }
 
         /// <summary>
         /// Starts scanning for BLE devices.
         /// </summary>
-        /// <returns></returns>
+        /// <returns>A task that represents the asynchronous read operation. The Task will finish after the scan has ended.</returns>
         Task StartScanningForDevicesAsync();
+
+        /// <summary>
+        /// Starts scanning for BLE devices.
+        /// </summary>
+        /// <param name="cancellationToken">The token to monitor for cancellation requests. The default value is None.</param>
+        /// <returns>A task that represents the asynchronous read operation. The Task will finish after the scan has ended.</returns>
         Task StartScanningForDevicesAsync(CancellationToken cancellationToken);
+
+        /// <summary>
+        /// Starts scanning for BLE devices that advertise the services included in <paramref name="serviceUuids"/>.
+        /// </summary>
+        /// <param name="serviceUuids">Requested service Ids.</param>
+        /// <returns>A task that represents the asynchronous read operation. The Task will finish after the scan has ended.</returns>
         Task StartScanningForDevicesAsync(Guid[] serviceUuids);
+
+        /// <summary>
+        /// Starts scanning for BLE devices that advertise the services included in <paramref name="serviceUuids"/>.
+        /// </summary>
+        /// <param name="serviceUuids">Requested service Ids.</param>
+        /// <param name="cancellationToken">The token to monitor for cancellation requests. The default value is None.</param>
+        /// <returns>A task that represents the asynchronous read operation. The Task will finish after the scan has ended.</returns>
         Task StartScanningForDevicesAsync(Guid[] serviceUuids, CancellationToken cancellationToken);
+
+        /// <summary>
+        /// Stops scanning for BLE devices.
+        /// </summary>
+        /// <returns>A task that represents the asynchronous read operation. The Task will finish after the scan has ended.</returns>
         Task StopScanningForDevicesAsync();
 
+        /// <summary>
+        /// Connects to the <paramref name="device"/>.
+        /// </summary>
+        /// <param name="device">Device to connect to.</param>
+        /// <param name="autoconnect">Android only: Automatically try to reconnect to the device, after the connection got lost.</param>
+        /// <returns>A task that represents the asynchronous read operation. The Task will finish after the device has been connected successfuly.</returns>
+        /// <exception cref="DeviceConnectionException">Thrown if the device connection fails.</exception>
         Task ConnectToDeviceAync(IDevice device, bool autoconnect = false);
 
         /// <summary>
-        /// 
+        /// Connects to the <paramref name="device"/>.
         /// </summary>
-        /// <param name="device"></param>
-        /// <param name="autoconnect"></param>
-        /// <param name="cancellationToken"></param>
-        /// <returns></returns>
-        /// <exception cref="DeviceConnectionException">Thrown if the device cónnection fails.</exception>
+        /// <param name="device">Device to connect to.</param>
+        /// <param name="autoconnect">Android only: Automatically try to reconnect to the device, after the connection got lost.</param>
+        /// <param name="cancellationToken">The token to monitor for cancellation requests. The default value is None.</param>
+        /// <returns>A task that represents the asynchronous read operation. The Task will finish after the device has been connected successfuly.</returns>
+        /// <exception cref="DeviceConnectionException">Thrown if the device connection fails.</exception>
         Task ConnectToDeviceAync(IDevice device, bool autoconnect, CancellationToken cancellationToken);
+
+        /// <summary>
+        /// Disconnects from the <paramref name="device"/>.
+        /// </summary>
+        /// <param name="device">Device to connect from.</param>
+        /// <returns>A task that represents the asynchronous read operation. The Task will finish after the device has been disconnected successfuly.</returns>
         Task DisconnectDeviceAsync(IDevice device);
     }
 }
