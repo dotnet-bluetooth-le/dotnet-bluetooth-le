2ea56d93c7a51769af99108e6cee5fc2dcd49b47
sead <sead@zuehlke.com>
2016-05-13 11:02:18 +0200

    Added compiler please check. #27

---
+++ .build/PluginNugetTest/PluginNugetTest.Android/CompilerPleaseCheck.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Plugin.BLE.Abstractions.Contracts;
+using Plugin.BLE.Android;
+using IAdapter = Plugin.BLE.Abstractions.Contracts.IAdapter;
+
+namespace PluginNugetTest.Android
+{
+    public class CompilerPleaseCheck
+    {
+        public void CheckMyAdapter(IAdapter adapter)
+        {
+            adapter.StartScanningForDevicesAsync();
+        }
+
+        public void CheckMyDevice(IDevice device)
+        {
+            device.UpdateRssiAsync();
+        }
+
+        public void CheckMyCharacteristic(ICharacteristic characteristic)
+        {
+            characteristic.StartUpdates();
+        }
+
+        public void CheckMyService(IService service)
+        {
+            service.GetCharacteristicAsync(Guid.Empty);
+        }
+
+        public async void CheckMyAdapter(Adapter adapter)
+        {
+            await adapter.StartScanningForDevicesAsync();
+        }
+
+        public async void CheckMyDevice(Device device)
+        {
+            await device.UpdateRssiAsync();
+        }
+
+        public void CheckMyCharacteristic(Characteristic characteristic)
+        {
+            characteristic.StartUpdates();
+        }
+
+        public async void CheckMyService(Service service)
+        {
+            await service.GetCharacteristicAsync(Guid.Empty);
+        }
+    }
+}
---
+++ .build/PluginNugetTest/PluginNugetTest/CompilerPleaseCheck.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Plugin.BLE.Abstractions.Contracts;
+
+namespace PluginNugetTest
+{
+    public class CompilerPleaseCheck
+    {
+        public void CheckMyAdapter(IAdapter adapter)
+        {
+            adapter.StartScanningForDevicesAsync();
+        }
+
+        public void CheckMyDevice(IDevice device)
+        {
+            device.UpdateRssiAsync();
+        }
+
+        public void CheckMyCharacteristic(ICharacteristic characteristic)
+        {
+            characteristic.StartUpdates();
+        }
+
+        public void CheckMyService(IService service)
+        {
+            service.GetCharacteristicAsync(Guid.Empty);
+        }
+    }
+}
