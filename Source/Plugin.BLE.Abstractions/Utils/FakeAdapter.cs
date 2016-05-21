866b68ad24836a36f49f5417bb6b0f5027fc5757
Sven-Michael Stübe <stuebe2k7@gmail.com>
2016-05-21 20:37:38 +0200

    don't ignore cs files with fake in its name :rage1:

---
+++ Source/Plugin.BLE.Abstractions/Utils/FakeAdapter.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Plugin.BLE.Abstractions.Contracts;
+
+namespace Plugin.BLE.Abstractions.Utils
+{
+    internal class FakeAdapter : AdapterBase
+    {
+        public override IList<IDevice> ConnectedDevices { get; } = new List<IDevice>();
+        protected override Task StartScanningForDevicesNativeAsync(Guid[] serviceUuids, CancellationToken scanCancellationToken)
+        {
+            TraceUnavailability();
+            return Task.FromResult(0);
+        }
+
+        protected override void StopScanNative()
+        {
+            TraceUnavailability();
+        }
+
+        protected override Task ConnectToDeviceNativeAync(IDevice device, bool autoconnect, CancellationToken cancellationToken)
+        {
+            TraceUnavailability();
+            return Task.FromResult(0);
+        }
+
+        protected override void DisconnectDeviceNative(IDevice device)
+        {
+            TraceUnavailability();
+        }
+
+        private static void TraceUnavailability()
+        {
+            Trace.Message("Bluetooth LE is not available on this device. Nothing will happen - ever!");
+        }
+    }
+}
