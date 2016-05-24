db97a7b4e36b1c953cc78cce1bc77a3c1b7d5f18
Sven-Michael Stübe <stuebe2k7@gmail.com>
2016-05-24 23:01:24 +0200

    forgot to save #40

--- Source/Plugin.BLE.Abstractions/Contracts/IBluetoothLE.cs
+++ Source/Plugin.BLE.Abstractions/Contracts/IBluetoothLE.cs
@@ -1,36 +1,28 @@
 using System;
+using Plugin.BLE.Abstractions.EventArgs;
 
 namespace Plugin.BLE.Abstractions.Contracts
 {
     public interface IBluetoothLE
     {
+        /// <summary>
+        /// Occurs when <see cref="State"/> has changed.
+        /// </summary>
         event EventHandler<BluetoothStateChangedArgs> StateChanged;
         BluetoothState State { get; }
+        /// <summary>
+        /// Indicates whether the device can communicate via bluetooth low energy.
+        /// </summary>
         bool IsAvailable { get; }
+        /// <summary>
+        /// Indicates whether the bluetooth adapter is turned on or not.
+        /// <c>true</c> if <see cref="State"/> is <c>BluetoothState.On</c>
+        /// </summary>
         bool IsOn { get; }
 
-
+        /// <summary>
+        /// Adapter to that provides access to the physical bluetooth adapter.
+        /// </summary>
         IAdapter Adapter { get; }
-    }
-
-    public enum BluetoothState
-    {
-        Unknown,
-        Unavailable,
-        Unauthorized,
-        TurningOn,
-        On,
-        TurningOff,
-        Off
-    }
-
-    public class BluetoothStateChangedArgs
-    {
-        public BluetoothState NewState { get; }
-
-        public BluetoothStateChangedArgs(BluetoothState newState)
-        {
-            NewState = newState;
-        }
     }
 }
