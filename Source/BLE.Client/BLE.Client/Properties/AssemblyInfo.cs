2fdd72744fa52cbeaa28c90c3ea3fd0966400396
Sven-Michael Stübe <stuebe2k7@gmail.com>
2017-04-04 20:28:20 +0200

    bumped version to 1.2.2

--- Source/BLE.Client/BLE.Client/Properties/AssemblyInfo.cs
+++ Source/BLE.Client/BLE.Client/Properties/AssemblyInfo.cs
@@ -26,7 +26,7 @@ using Xamarin.Forms.Xaml;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.2.1")]
-[assembly: AssemblyVersion("1.2.1")]
-[assembly: AssemblyFileVersion("1.2.1")]
+// [assembly: AssemblyVersion("1.2.2")]
+[assembly: AssemblyVersion("1.2.2")]
+[assembly: AssemblyFileVersion("1.2.2")]
 [assembly: XamlCompilation(XamlCompilationOptions.Compile)]
