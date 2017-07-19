e5d62d383677d8f582844ab3bdd32dba518e077b
kobywchan <kobychan@stanford.edu>
2017-07-19 13:56:22 -0700

    Windows implemented
    Full functionality of Plugin for UWP including connect, scan, get services/descriptors/characteristics, read and write characteristics/descriptors, event handlers, accessor methods, etc.
    
    Known Issues for UWP:
    -Disconnect cannot be implemented with UWP API
    -GetSystemConnectedOrPairedDevices cannot be implemented without being async
    -GetInitialState only returns connected or disconnected (also due to UWP API limitations)
    -RequestMtu is not supported in windows
    -Rssi cannot be updated since Rssi information is only recieved in nativeadvertisements
    -UpdateConnectionInterval is not supported in UWP
    -IsPrimary returns true as default since the method to get parent services is now obselete in the UWP API

---
+++ Source/Plugin.BLE.UWP/CrossBluetoothLE.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Plugin.BLE.Abstractions.Contracts;
+
+namespace Plugin.BLE
+{
+    /// <summary>
+    /// Cross platform bluetooth LE implemenation.
+    /// </summary>
+    public static class CrossBluetoothLE
+    {
+        static readonly Lazy<IBluetoothLE> Implementation = new Lazy<IBluetoothLE>(CreateImplementation, System.Threading.LazyThreadSafetyMode.PublicationOnly);
+
+        /// <summary>
+        /// Current bluetooth LE implementation.
+        /// </summary>
+        public static IBluetoothLE Current
+        {
+            get
+            {
+                var ret = Implementation.Value;
+                if (ret == null)
+                {
+                    throw NotImplementedInReferenceAssembly();
+                }
+                return ret;
+            }
+        }
+
+        static IBluetoothLE CreateImplementation()
+        {
+#if PORTABLE
+            return null;
+#else
+            var implementation = new BleImplementation();
+            implementation.Initialize();
+            return implementation;
+#endif
+        }
+
+        internal static Exception NotImplementedInReferenceAssembly()
+        {
+            return new NotImplementedException("This functionality is not implemented in the portable version of this assembly.  You should reference the NuGet package from your main application project in order to reference the platform-specific implementation.");
+        }
+    }
+}
