bb57ff6823ec1016bd554c7da593078f575fd0f0
Felix.x <flyingxu@gmail.com>
2018-05-16 15:07:57 +0800

    BLE.Client.Droid

--- Source/BLE.Client/BLE.Client.Droid/Bootstrap/BlePluginBootstrap.cs
+++
@@ -1,9 +0,0 @@
-﻿using MvvmCross.Platform.Plugins;
-
-namespace BLE.Client.Droid.Bootstrap
-{
-    public class BlePluginBootstrap
-        : MvxPluginBootstrapAction<MvvmCross.Plugins.BLE.PluginLoader>
-    {
-    }
-}
