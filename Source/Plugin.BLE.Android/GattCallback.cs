1e215a61cd33be58ffcd2d6fe7508efe94af5f8f
Ulrich <ulbo@bluefragments.com>
2019-07-02 13:10:10 +0200

    CLOUD-8204 add logging

--- Source/Plugin.BLE.Android/GattCallback.cs
+++ Source/Plugin.BLE.Android/GattCallback.cs
@@ -136,6 +136,7 @@ namespace Plugin.BLE.Android
                     {
                         if (gatt.Device.Address == null || _device == null)
                         {
+                            Trace.Info($"Address or device is null address is: {gatt.Device.Address} device is: {_device} GattStatus is: {status}");
                             return;
                         }
 
--- Source/Plugin.BLE.iOS/DefaultTrace.cs
+++ Source/Plugin.BLE.iOS/DefaultTrace.cs
@@ -1,13 +1,38 @@
 ﻿using System;
+using BC.Mobile.Logging;
 using Plugin.BLE.Abstractions;
 
 namespace Plugin.BLE
 {
     static class DefaultTrace
     {
+        private static readonly ILogger _logger = LoggerFactory.CreateLogger("Plugin.BLE");
+
         static DefaultTrace()
         {
-            Trace.TraceImplementation = Console.WriteLine;
+            Trace.TraceDebugImplementation = (parameter, args) =>
+            {
+                try
+                {
+                    _logger.Debug(() => string.Format(parameter, args));
+                }
+                catch (Exception ex)
+                {
+                    _logger.Debug(() => $"Failed to format string with arguments, ex: {ex.Message}");
+                }
+            };
+
+            Trace.TraceInfoImplementation = (parameter, args) =>
+            {
+                try
+                {
+                    _logger.Info(string.Format(parameter, args));
+                }
+                catch (Exception ex)
+                {
+                    _logger.Info($"Failed to format string with arguments, ex: {ex.Message}");
+                }
+            };
         }
     }
 }
