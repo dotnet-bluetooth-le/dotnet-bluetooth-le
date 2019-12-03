713d827a93ba12cad4045022ce6c1ac37e766df0
Ulrich <ulbo@bluefragments.com>
2019-12-03 16:24:43 +0100

    More cancellation tokens

--- Source/Plugin.BLE.Abstractions/AdapterBase.cs
+++ Source/Plugin.BLE.Abstractions/AdapterBase.cs
@@ -90,7 +90,7 @@ namespace Plugin.BLE.Abstractions
             return Task.FromResult(0);
         }
 
-        public Task<IDevice> ConnectAsync(string friendlyName, Guid uuid, Func<IDevice, bool> deviceFilter, CancellationToken cancellationToken = default)
+        public Task<IDevice> ConnectAsync(string friendlyName, Guid uuid, Func<IDevice, bool> deviceFilter, CancellationToken cancellationToken = default(CancellationToken))
         {
             return ConnectNativeAsync(friendlyName, uuid, deviceFilter, cancellationToken);
         }
@@ -239,7 +239,7 @@ namespace Plugin.BLE.Abstractions
         protected abstract void StopScanNative();
         protected abstract Task ConnectToDeviceNativeAsync(IDevice device, ConnectParameters connectParameters, CancellationToken cancellationToken);
         protected abstract void DisconnectDeviceNative(IDevice device);
-        protected abstract Task<IDevice> ConnectNativeAsync(string friendlyName, Guid uuid, Func<IDevice, bool> deviceFilter, CancellationToken cancellationToken = default);
+        protected abstract Task<IDevice> ConnectNativeAsync(string friendlyName, Guid uuid, Func<IDevice, bool> deviceFilter, CancellationToken cancellationToken = default(CancellationToken));
 
         public abstract Task<IDevice> ConnectToKnownDeviceAsync(Guid deviceGuid, ConnectParameters connectParameters = default(ConnectParameters), CancellationToken cancellationToken = default(CancellationToken));
         public abstract List<IDevice> GetSystemConnectedOrPairedDevices(Guid[] services = null);
