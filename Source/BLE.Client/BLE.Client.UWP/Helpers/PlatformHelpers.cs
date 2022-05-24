43b1b413a043e12ed1bfcda8b0bcf27eaadd97fe
fozzzgate <31154558+fozzzgate@users.noreply.github.com>
2022-05-24 08:13:42 -0700

    Fix for BLE.Client.UWP 'Scan' null-reference crash
    Caused by missing UWP CheckAndRequestBluetoothPermissions function

---
+++ Source/BLE.Client/BLE.Client.UWP/Helpers/PlatformHelpers.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using BLE.Client.Helpers;
+
+[assembly: Dependency(typeof(BLE.Client.UWP.Helpers.PlatformHelpers))]
+namespace BLE.Client.UWP.Helpers
+{
+    public class PlatformHelpers : IPlatformHelpers
+    {
+        public Task<PermissionStatus> CheckAndRequestBluetoothPermissions()
+        {
+            return Task.FromResult(PermissionStatus.Granted);
+        }
+    }
+}
