e31639e9620dcd53d042d8568b53e303c4db9fb0
Janus Weil <janus@gcc.gnu.org>
2023-04-20 15:09:20 +0200

    fix compiler warnings in KnownCharacteristic
    * Missing XML comment for publicly visible type or member ...

--- Source/Plugin.BLE/Shared/KnownCharacteristic.cs
+++ Source/Plugin.BLE/Shared/KnownCharacteristic.cs
@@ -2,11 +2,23 @@ using System;
 
 namespace Plugin.BLE.Abstractions
 {
+    /// <summary>
+    /// Information about a known characteristic (name and ID).
+    /// </summary>
     public struct KnownCharacteristic
     {
+        /// <summary>
+        /// Name of the characteristic. 
+        /// </summary>
         public string Name { get; private set; }
+        /// <summary>
+        /// Id of the characteristic.
+        /// </summary>
         public Guid Id { get; private set; }
 
+        /// <summary>
+        /// Constructor.
+        /// </summary>
         public KnownCharacteristic(string name, Guid id)
         {
             Name = name;
