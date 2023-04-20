27a8edd55c32247f42d95689a0e59bb7796c3c64
Janus Weil <janus@gcc.gnu.org>
2023-04-20 13:52:28 +0200

    fix compiler warnings in DeviceBondStateChangedEventArgs
    * Missing XML comment for publicly visible type or member ...

--- Source/Plugin.BLE/Shared/EventArgs/DeviceBondStateChangedEventArgs.cs
+++ Source/Plugin.BLE/Shared/EventArgs/DeviceBondStateChangedEventArgs.cs
@@ -2,9 +2,18 @@ using Plugin.BLE.Abstractions.Contracts;
 
 namespace Plugin.BLE.Abstractions.EventArgs
 {
+    /// <summary>
+    /// Event arguments for <c>BondStatusBroadcastReceiver.BondStateChanged</c>
+    /// </summary>
     public class DeviceBondStateChangedEventArgs : System.EventArgs
     {
+        /// <summary>
+        /// The device.
+        /// </summary>
         public IDevice Device { get; set; }
+        /// <summary>
+        /// The bond state.
+        /// </summary>
         public DeviceBondState State { get; set; }
     }
 }
