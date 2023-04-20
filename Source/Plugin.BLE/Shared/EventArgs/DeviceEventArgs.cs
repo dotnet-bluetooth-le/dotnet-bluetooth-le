c2beaa0c00b27b8e6b6e511cc69516090fc5addc
Janus Weil <janus@gcc.gnu.org>
2023-04-20 14:03:36 +0200

    fix compiler warnings in DeviceEventArgs
    * Missing XML comment for publicly visible type or member ...

--- Source/Plugin.BLE/Shared/EventArgs/DeviceEventArgs.cs
+++ Source/Plugin.BLE/Shared/EventArgs/DeviceEventArgs.cs
@@ -2,8 +2,15 @@ using Plugin.BLE.Abstractions.Contracts;
 
 namespace Plugin.BLE.Abstractions.EventArgs
 {
+    /// <summary>
+    /// Event arguments for device events in <c>IAdapter</c>,
+    /// e.g. <c>DeviceAdvertised</c>, <c>DeviceDiscovered</c>, <c>DeviceConnected</c> and <c>DeviceDisconnected</c>
+    /// </summary>
     public class DeviceEventArgs : System.EventArgs
     {
+        /// <summary>
+        /// The device.
+        /// </summary>
         public IDevice Device;
     }
 }
