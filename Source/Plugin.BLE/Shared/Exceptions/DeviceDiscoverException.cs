d43dbf4d67391ceeaa447dc2977b09d8e9c77fd1
Janus Weil <janus@gcc.gnu.org>
2023-04-20 14:27:06 +0200

    fix compiler warnings in DeviceDiscoverException
    * Missing XML comment for publicly visible type or member ...

--- Source/Plugin.BLE/Shared/Exceptions/DeviceDiscoverException.cs
+++ Source/Plugin.BLE/Shared/Exceptions/DeviceDiscoverException.cs
@@ -2,8 +2,14 @@
 
 namespace Plugin.BLE.Abstractions.Exceptions
 {
+    /// <summary>
+    /// An exception that is thrown whenever a problem occurs with discovering a device.
+    /// </summary>
     public class DeviceDiscoverException : Exception
     {
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
         public DeviceDiscoverException() : base("Could not find the specific device.")
         {
         }
