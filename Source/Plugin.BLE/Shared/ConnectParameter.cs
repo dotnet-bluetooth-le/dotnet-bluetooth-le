0567c7685d5721235877461afa89441647e683da
alex3696 <alex3696@yandex.ru>
2026-05-09 03:05:06 +0700

    fix #877 - add option to ConnectParameters: CheckIsLeDeviceType (#994)
    * fix #877 - add option to ConnectParameters CheckIsLeDeviceType? and optional check NativeDevice.SupportsBLE before ConnectGatt
    
    * fix indentations and add explanations to the parameter ConnectParameter.CheckIsLeDeviceType
    
    * fix indentations
    
    * fixed the forgotten ability to change a parameter, change descriptions
    
    * make ConnectParameters readonly, add init setters to all members, update constuctor
    
    ---------
    
    Co-authored-by: Janus Weil <janus@gcc.gnu.org>
    (cherry picked from commit a69b4fc73bd7aa723fe53e74680b99b37e06d468)

--- Source/Plugin.BLE/Shared/ConnectParameter.cs
+++ Source/Plugin.BLE/Shared/ConnectParameter.cs
@@ -3,24 +3,33 @@
     /// <summary>
     /// Connection parameters. Contains platform specific parameters needed to achieved connection
     /// </summary>
-    public struct ConnectParameters
+    public readonly struct ConnectParameters
     {
         /// <summary>
         /// Android only, from documentation:  
         /// boolean: Whether to directly connect to the remote device (false) or to automatically connect as soon as the remote device becomes available (true).
         /// </summary>
-        public bool AutoConnect { get; }
+        public bool AutoConnect { get; init; } = false;
 
         /// <summary>
         /// Android only: For Dual Mode device, force transport mode to LE. The default is false.
         /// </summary>
-        public bool ForceBleTransport { get; }
+        public bool ForceBleTransport { get; init; } = false;
+
+        /// <summary>
+        /// Android only: Strict BluetoothDeviceType checking.
+        /// The connection will only be attempted if the device supports LE, otherwise a DeviceConnectionException will be thrown.
+        /// This check is an early “warning” of what might happen next - error GATT 133 or Bluetooth stack fault.
+        /// The BluetoothDeviceType may be Unknown immediately after the device is rebooted, or if the type is not accepted correctly, try scanning to get or update the type.
+        /// If the device intentionally does not declare the connection type, you can disable this check.
+        /// </summary>
+        public bool CheckIsLeDeviceType { get; init; } = false;
 
         /// <summary>
         /// Windows only, mapped to:
         /// https://learn.microsoft.com/en-us/uwp/api/windows.devices.bluetooth.bluetoothlepreferredconnectionparameters
         /// </summary>
-        public ConnectionParameterSet ConnectionParameterSet { get; }
+        public ConnectionParameterSet ConnectionParameterSet { get; init; } = ConnectionParameterSet.None;
 
         /// <summary>
         /// Default-constructed connection parameters (all parameters set to false).
@@ -33,14 +42,17 @@
         /// <param name="autoConnect">Android only: Whether to directly connect to the remote device (false) or to automatically connect as soon as the remote device becomes available (true). The default is false.</param>
         /// <param name="forceBleTransport">Android only: For Dual Mode device, force transport mode to LE. The default is false.</param>
         /// <param name="connectionParameterSet">Windows only: Default is None, where this has no effect - use eg. ThroughputOptimized for firmware upload to a device</param>
+        /// <param name="checkIsLeDeviceType">Android only: Check if device supports LE. The default is false.</param>
         public ConnectParameters(
             bool autoConnect = false,
             bool forceBleTransport = false,
-            ConnectionParameterSet connectionParameterSet = ConnectionParameterSet.None)
+            ConnectionParameterSet connectionParameterSet = ConnectionParameterSet.None,
+            bool checkIsLeDeviceType = false)
         {
             AutoConnect = autoConnect;
             ForceBleTransport = forceBleTransport;
             ConnectionParameterSet = connectionParameterSet;
+            CheckIsLeDeviceType = checkIsLeDeviceType;
         }
     }
 }
